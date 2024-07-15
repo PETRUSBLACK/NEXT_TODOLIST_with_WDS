37d6ebbedb7d4d1f2d018130e06d678554bf02b2
PETRUSBLACK <onukwupetrusoge@gmail.com>
2024-07-15 10:46:54 +0100

    initial commit

---
+++ app/new/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Page(){
+    return (
+    <div>
+        <div className="text-slate-300">This is the new Page</div>    
+    </div>
+    )
+}
